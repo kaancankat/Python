d1a66fae8d609ac6ee143ee09cc18272cf405634
kaancankat <kaancankat1@gmail.com>
2023-07-02 19:35:23 +0300

    update

--- gun2.py
+++ gun2.py
@@ -1,2 +1,14 @@
-name = "kaan cankat"
-print(len(name))
+    #gün 2
+#name = "kaan cankat"
+#print(len(name))
+
+#myString="hello python"
+#print(myString[2])
+     #gün 3
+#myName="kaan cankat"
+#print(len(myName)-1)
+#print(myName[-1])
+
+#arcode = "ABCDFJ12345"
+#print(barcode[3:])
+
