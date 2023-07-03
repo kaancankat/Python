b9aee81751486f992d719bf4106a409355f4e00b
kaancankat <kaancankat1@gmail.com>
2023-07-03 11:33:31 +0300

    update

---
+++ gun4.py
@@ -0,0 +1,25 @@
+#sözlük(dictionary)
+#fruit_list=["banana,apple"]
+#calorie_list=[100,150]
+#fitness_dictionary ={"banana":100,"apple":150}
+#fitness_dictionary["banana"] = 200
+#fitness_dictionary["melon"] =300
+#fitness_dictionary.get("appl","yanlış yazdınız")
+#print(fitness_dictionary.get("appl"))
+#print(fitness_dictionary)
+#my_dictionary={"k1":10,"k2":[10,20,30,40,50],"k3":"string","k4":{"a":100,"b":200}}
+#print(my_dictionary["k4"]["b"]) 
+#sets
+#mylist=[10,"a","b",40]
+#myset = set(mylist)
+#print(myset)
+#emptySet =set()
+#emptySet.add(10)
+#emptySet.add(10)
+#emptySet.add(10)
+#emptySet.add(10)
+#emptySet.add(20)
+#emptySet.add(20)
+#print(emptySet)
+#age = int(input("yaşınızı giriniz"))
+#print(age>18)
