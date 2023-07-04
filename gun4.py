8fe6dc5c199efad9501fb4832ac66983a716ad61
kaancankat <kaancankat1@gmail.com>
2023-07-04 11:03:40 +0300

    update

--- gun4.py
+++ gun4.py
@@ -23,3 +23,17 @@
 #print(emptySet)
 #age = int(input("yaşınızı giriniz"))
 #print(age>18)
+my_string = "Python Ogreniyorum"
+a=my_string[5]
+print(a)
+my_new_string = "ProgramlamayaMerhabaDedik"
+print(my_new_string[5:8])
+my_last_string = "Afyonkarahisarlilastiramadiklarimizdanmisiniz"
+print(my_last_string[::-1])
+print((7*12)+5)
+my_list = [3.14,4,[2,3,"b"],True]
+print(my_list[2][2])
+my_dictionary = {"key1":20.25, "kk2":[40,{"k21":"a"}]}
+print(my_dictionary["kk2"][1]["k21"])
+my_list_to_be_set = [3,4,9,3,21,22,4,3,9,10,21,22]
+print(set(my_list_to_be_set))
