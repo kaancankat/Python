92b4daaf5735f0f35f689a5f51dceca97f1b52cd
kaancankat <kaancankat1@gmail.com>
2023-07-06 21:19:08 +0300

    update

---
+++ gun6.py
@@ -0,0 +1,14 @@
+#my_superhero = "batman"
+#my_superhero == "batman"
+#if my_superhero == "batman":
+#    print("batmann")
+#else:
+#    print("ironman")
+#a = input("süper kahraman giriniz")
+#if a == "superman":
+ #   print("superman")
+#elif a == "batman":
+ #   print("aquaman")
+#else:
+#    print(":(")
+#for number inmy list
