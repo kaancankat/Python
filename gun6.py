a668e34b1beef2cf30c31b161784a17da30b2dce
kaancankat <kaancankat1@gmail.com>
2023-07-07 15:23:19 +0300

    update

--- gun6.py
+++ gun6.py
@@ -12,3 +12,17 @@
 #else:
 #    print(":(")
 #for number inmy list
+#my_tuple=[10,20,30,40,50,60,70]
+#for num in my_tuple:
+#    print(num / 5*2)
+#mylist=[("a","b"),("c","d"),("e","f"),("g","h")]
+#for (x,y) in mylist:
+#    print(x)
+#for number in my_tuple:
+#    print(number)
+#    if number == 40:
+#        print("yes")
+#        break
+
+#for num in my_tuple:
+#    pass
