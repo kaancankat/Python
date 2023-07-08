e391c273693d3b132757ed9fa59a064afe63f6a4
kaancankat <kaancankat1@gmail.com>
2023-07-08 13:52:44 +0300

    update

--- gun7.py
+++ gun7.py
@@ -1,4 +1,27 @@
-x=0
-while x < 10 :
-    print(x)
-    x=x+1
+#x=0
+#while x < 10 :
+#    print(x)
+#    x=x+1
+#last_list =[10,20,30,40,50]
+#while 20 in last_list:
+#    print("20 in last_list")
+#    last_list.pop()
+
+#list=[10,20,30,40]
+#print(list(range(5,25)))
+#print(list(range(5,25,2)))
+#for num in range(len(list)):
+#    print(num)
+#for a in enumerate(list):
+#    print(a)
+
+#from random import randint
+#print(randint(0,100))
+#from random import shuffle
+#print(shuffle(list))
+
+#food_list = ["apple","banana","melon"]
+#calories=[100,150,200]
+#day=["mon","tuesday","wed"]
+#zippedlist=list(zip(food_list,calories,day))
+#print(zippedlist)
