3be4d0ec15032b0abaffeabaf6588605abd571f9
kaancankat <kaancankat1@gmail.com>
2023-07-09 12:48:38 +0300

    update

---
+++ gun8.py
@@ -0,0 +1,24 @@
+#my_dictionary = {"k1":10,"k2k":"a","k32":30,"k4":"c"}
+#if "c" in my_dictionary["k4"]:
+#    print("c harfi vardır")
+#else:
+#    print("yoktur")
+#my_other_dictionary = {"b":203,"c":"a","a":400,"d":"f"}
+#if "a" in my_other_dictionary.keys():
+#    print("vardır")
+#else:
+#    print("yoktur")
+#my_numbers = [1,2,3,4,5,6,19,20,32,21,20,1111,23,24]
+#for i in my_numbers:
+#    if i%2 == 0:
+#        print(i)
+metal_list = ["Metallica","Iron Maiden","Dream Theater","Megadeth","AC/DC"]
+from random import shuffle
+shuffle(metal_list)
+print(metal_list)
+r_list = [3,2,5,8,4,6,9,12]
+pi=3.14
+cevreList=[]
+for r in r_list:
+    cevreList.append(2*pi*r)
+print(cevreList)
