6ff278663b74a94c211f44aa9ef2315efd12bf87
kaancankat <kaancankat1@gmail.com>
2023-07-10 12:05:40 +0300

    update

---
+++ gun9.py
@@ -0,0 +1,61 @@
+#myName="kaan"
+#def hellp_python():
+#    print("hello")
+#    print("python")
+#    
+#hellp_python()
+#
+#def hello_name(name):
+#    print("hello")
+#    print(name)
+#hello_name("Kaan")
+#def sem_ex(num1,num2):
+#    print(num1+num2)
+#sem_ex(10,5)
+##
+###def summation(num1,num2,num3):
+#    return num1+num2+num3
+#x=summation(5,2,6)
+#print(x)
+#
+#def controlString(s):
+#    if s[0]=="a":
+#        print("a")
+#    else:
+#        print("a ile başlamıyor")
+#controlString("kaan")
+#def args_sum(*args):
+##    return sum(args)
+##print(args_sum(333,134))
+##
+##def kwargs_ex(**kwargs):
+##    print(kwargs)
+##kwargs_ex(apple=100,banana=200)
+def divideNmber(number):
+    return number /2
+myList=[3,5,7,10,20,30]
+List2=[]
+
+for r in myList:
+    x = divideNmber(r)
+    List2.append(x)
+print(List2)
+
+list(map(divideNmber,myList))
+
+def controling(string):
+    return "kaan" in string
+
+myStringlist=["kaan","kaan Cankat","arda","kerem"]
+
+list(filter(controling,myStringlist))
+
+list(map(controling,myStringlist))
+
+lambdaaa = lambda num : num *3
+lambdaaa(20)
+result = lambdaaa(10)
+
+numList=[10,20,30,40,50]
+
+list(map(lambda numm: num/4,numList))
