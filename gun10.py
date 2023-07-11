d331294f4060375f1fd9aa14f887fbd562ccf476
kaancankat <kaancankat1@gmail.com>
2023-07-11 14:32:32 +0300

    update

---
+++ gun10.py
@@ -0,0 +1,18 @@
+#atilName ="atil"
+#atilAge = 90
+#atilGender ="male"
+#atlasage = 10
+
+class Person():
+    name = ""
+    age=""
+    
+    def __init__(self,nameInput,ageInput,genderInput):
+        self.name=nameInput
+        self.name=ageInput
+        self.name=genderInput
+        print("çalıştı")
+myList=list()
+
+atil =Person()
+
--- gun9.py
+++ gun9.py
@@ -19,48 +19,48 @@
 #print(x)
 #
 #def controlString(s):
-#    if s[0]=="a":
-#        print("a")
-#    else:
-#        print("a ile başlamıyor")
-#controlString("kaan")
-#def args_sum(*args):
-##    return sum(args)
-##print(args_sum(333,134))
-##
-##def kwargs_ex(**kwargs):
-##    print(kwargs)
-##kwargs_ex(apple=100,banana=200)
-def divideNmber(number):
-    return number /2
-myList=[3,5,7,10,20,30]
-List2=[]
-
-for r in myList:
-    x = divideNmber(r)
-    List2.append(x)
-print(List2)
-
-list(map(divideNmber,myList))
-
-def controling(string):
-    return "kaan" in string
-
-myStringlist=["kaan","kaan Cankat","arda","kerem"]
-
-list(filter(controling,myStringlist))
-
-list(map(controling,myStringlist))
-
-lambdaaa = lambda num : num *3
-lambdaaa(20)
-result = lambdaaa(10)
-
-numList=[10,20,30,40,50]
-
-list(map(lambda numm: num/4,numList))
-
-x=20
-def multiply(numm):
-    x = 5
-    return numm * x 
+##    if s[0]=="a":
+##        print("a")
+##    else:
+##        print("a ile başlamıyor")
+##controlString("kaan")
+##def args_sum(*args):
+###    return sum(args)
+###print(args_sum(333,134))
+###
+###def kwargs_ex(**kwargs):
+###    print(kwargs)
+###kwargs_ex(apple=100,banana=200)
+#def divideNmber(number):
+#    return number /2
+#myList=[3,5,7,10,20,30]
+#List2=[]
+#
+#for r in myList:
+#    x = divideNmber(r)
+#    List2.append(x)
+#print(List2)
+#
+#list(map(divideNmber,myList))
+#
+#def controling(string):
+#    return "kaan" in string
+#
+#myStringlist=["kaan","kaan Cankat","arda","kerem"]
+#
+#list(filter(controling,myStringlist))
+#
+#list(map(controling,myStringlist))
+#
+#lambdaaa = lambda num : num *3
+#lambdaaa(20)
+#result = lambdaaa(10)
+#
+#numList=[10,20,30,40,50]
+#
+#list(map(lambda numm: num/4,numList))
+#
+#x=20
+#def multiply(numm):
+#    x = 5
+#    return numm * x     
