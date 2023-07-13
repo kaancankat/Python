52253822e400fecab282c58191c0f83d7eb5bab3
kaancankat <kaancankat1@gmail.com>
2023-07-13 12:18:19 +0300

    update

--- gun10.py
+++ gun10.py
@@ -3,19 +3,63 @@
 #atilGender ="male"
 #atlasage = 10
 
-class Person():
-    name = ""
-    age=""
-    
-    def __init__(self,nameInput,ageInput,genderInput):
-        self.name=nameInput
-        self.age=ageInput
-        print("çalıştı")
-myList=list()
-
-atil =Person()
-
-
-
-
-
+#class Person():
+#    name = ""
+#    age=""
+#    
+#    def __init__(self,nameInput,ageInput,genderInput):
+#        self.name=nameInput
+#        self.age=ageInput
+#        print("çalıştı")
+#myList=list()
+#
+#atil =Person()
+#
+#class dog():
+#    year = 7
+#    def __init__(self,age):
+#        self.age = age
+##    def humanAge(self):
+##        return self.age *self.year
+##mydog = dog(3)
+##print(mydog.age)
+##print(mydog.humanAge())
+#
+#class musican():
+#    
+#    def __init__(self,name):
+#        self.name = name
+#        print("musican class")
+#        
+#    def test1(self):
+#        print("test1")
+#    def test2(self):
+#        print("test2")
+#
+#class musicianPlus(musican):
+#    def __init__(self,name):
+#        musican.__init__(self,name)
+#        print("musician plus")
+#atlas = musicianPlus("atlas")
+#atlas.name = "atlas samancıoğlu"
+#
+#class banana():
+#    def __init__(self,name):
+#        self.name = name
+#    
+#    def info(self):
+#        return f"100 calories {self.name}"
+#class apple():
+#    def __init__(self,name):
+#        self.name = name
+#    
+#    def info(self):
+#        return f"150 calories {self.name}"
+#    
+#banana = banana("banana")
+#apple = apple("apple")
+#print(banana.info())
+#
+#fruitlist = [banana,apple]
+#for fruit in fruitlist:
+#    print(fruit.info())
---
+++ gun11.py
@@ -0,0 +1,4 @@
+class banana():
+    def __init__(self,name):
+        self.name = name
+     
