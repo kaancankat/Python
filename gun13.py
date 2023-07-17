40de58f98b0cf588354613744d6fa4e3087f9bdf
kaancankat <kaancankat1@gmail.com>
2023-07-17 13:44:57 +0300

    update

---
+++ gun13.py
@@ -0,0 +1,11 @@
+
+def example_func():
+    print("çalıştı")
+    
+example_func()
+
+#from animal package import info
+#from animalPackage.catpackage import meow
+
+if __name__ == "__main__":
+    print("çalıştı")
---
+++ pytube.py
@@ -0,0 +1,5 @@
+import pytube
+url = input("enter video url")
+path = ""
+pytube.youtube(url)streams.get_highest_resolution().download(path)
+
