a45a75d6323b061f2bd083040fc8749e412431d7
kaan cankat <kaancankat1@gmail.com>
2023-08-04 12:46:01 +0300

    update

--- turtlegame.py
+++ turtlegame.py
@@ -3,6 +3,17 @@ import turtle
 drawing_board = turtle.Screen()
 drawing_board.bgcolor("light blue")
 drawing_board.title("python Turtle")
+FONT= ("arial",30,"normal")
+#score turtle
+score_turtle = turtle.Turtle()
 
-turtle_instance = turtle.Turtle()
-turtle.shape("turtle")
+score_turtle.color("dark blue")
+score_turtle.hideturtle()
+score_turtle.write(arg="score: 0", move=False, align="center",font=FONT)
+
+
+
+
+
+
+turtle.mainloop()
