220c11debda31253c89da5c27e128386b15e03f4
kaan cankat <kaancankat1@gmail.com>
2023-08-06 12:29:52 +0300

    update

--- turtlegame.py
+++ turtlegame.py
@@ -5,7 +5,10 @@ drawing_board = turtle.Screen()
 drawing_board.bgcolor("light blue")
 drawing_board.title("python Turtle")
 FONT= ("arial",30,"normal")
-#score turtle
+score=0
+
+turtlelist=[]
+
 def score_table():
     score_turtle = turtle.Turtle()
     score_turtle.color("dark blue")
@@ -19,20 +22,43 @@ grid_size=10
 def make_turtle(x,y):
     t=turtle.Turtle()
     
+    def handle_click(x,y):
+        global score
+        score += 1
+        score_turtle.clear()
+        score_turtle.write(arg="score: {}".format(score), move=False, align="center",font=FONT)
     
+    t.onclick(handle_click)
     t.penup()
     t.shape("turtle")
     t.shapesize(2,2)
     t.color("dark green")
     t.goto(x*grid_size, y*grid_size)
+    turtlelist.append(t)
 
-make_turtle(-20,20)
-make_turtle(0,30)
-make_turtle(10,10)
-make_turtle(10,10)
-make_turtle(10,10)
-make_turtle(10,10)
+x_coordinates =[-20,-10,0,10,20]
+y_coordinates=[20,10,0,-10]
 
+def setup_turtle():
+    for x in x_coordinates:
+        for y in y_coordinates:
+            make_turtle(x,y)
+            
+def hide_turtless():
+    for t in turtlelist:
+        t.hideturtle()
+        
+def random_turtle():
+    random.choice(turtlelist).showturtle()
+        
+turtle.tracer(0)
 
+setup_turtle()
 score_table()
+hide_turtless()
+random_turtle()
+
+turtle.tracer(1)
+
+
 turtle.mainloop()
