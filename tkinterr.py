f91d3bee93c18f16c402177b46ff1d35b060fc8b
kaan cankat <kaancankat1@gmail.com>
2023-08-10 21:04:32 +0300

    update

--- tkinterr.py
+++ tkinterr.py
@@ -2,10 +2,24 @@ import tkinter
 
 window =tkinter.Tk()
 window.title("python Tkinter")
+window.minsize(width=450, height=300)
 
 #label
-my_label = tkinter.Label()
-my_label.config(bg="black")
+my_label = tkinter.Label(text="deneme")
+my_label.config(bg="black",fg="white")
+my_label.pack()
+
+def click_button():
+    user_input=my_entry.get()
+    print(user_input)
+
+#button
+my_button = tkinter.Button(text="this is a button", command=click_button)
+my_button.pack()
 
 
-window.mainloop(text="text")
+my_entry = tkinter.Entry(width=20)
+my_entry.pack()
+
+
+window.mainloop()
